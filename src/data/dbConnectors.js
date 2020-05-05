2d210f181cd4ab76f81320e43408174356d36f20
Ric-Lavers <ric_lavers@outlook.com>
2020-05-05 19:31:23 +1000

    updated env

--- src/data/dbConnectors.js
+++ src/data/dbConnectors.js
@@ -1,21 +1,31 @@
 require("dotenv").config()
 import mongoose from "mongoose"
 
-import { demoSchema, wordSchema, groupSchema } from "./mongoSchemas"
+import {
+  demoSchema,
+  wordSchema,
+  groupSchema
+} from "./mongoSchemas"
 
 var MONGO_URI
 if (process.env.APP_ENV === "development") {
-  MONGO_URI = process.env.DEVELOPMENT_MONGO_URI
+  MONGO_URI = process.env.MONGO_URI
 } else {
-  MONGO_URI = process.env.PROD_MONGO_URI_SKRIBBL
+  MONGO_URI = process.env.MONGO_URI
 }
 
 //Mongo connection
 mongoose.Promise = global.Promise
-mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true
+})
 
 const Demo = mongoose.model("demo", demoSchema)
 const Word = mongoose.model("word", wordSchema)
 const Group = mongoose.model("group", groupSchema)
 
-export { Demo, Word, Group }
+export {
+  Demo,
+  Word,
+  Group
+}
