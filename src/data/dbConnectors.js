0d4a0005a0c419e176ec4f84cd534c25a41cc575
Ric-Lavers <ric_lavers@outlook.com>
2020-05-05 20:56:00 +1000

    .

--- src/data/dbConnectors.js
+++ src/data/dbConnectors.js
@@ -1,31 +1,36 @@
-require("dotenv").config()
-import mongoose from "mongoose"
+require("dotenv").config();
+import mongoose from "mongoose";
 
 import {
   demoSchema,
   wordSchema,
   groupSchema
-} from "./mongoSchemas"
+} from "./mongoSchemas";
 
-var MONGO_URI
+var MONGO_URI = process.env.MONGO_URI;
+
 if (process.env.APP_ENV === "development") {
-  MONGO_URI = process.env.MONGO_URI
+  MONGO_URI = process.env.DEV_MONGO_URI;
 } else {
-  MONGO_URI = process.env.MONGO_URI
+  MONGO_URI = process.env.PROD_MONGO_URI;
 }
+console.log({
+  MONGO_URI
+});
 
 //Mongo connection
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const Demo = mongoose.model("demo", demoSchema)
-const Word = mongoose.model("word", wordSchema)
-const Group = mongoose.model("group", groupSchema)
+const Demo = mongoose.model("demo", demoSchema);
+const Word = mongoose.model("word", wordSchema);
+const Group = mongoose.model("group", groupSchema);
 
 export {
   Demo,
   Word,
   Group
-}
+};
