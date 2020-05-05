97aeb53705d13947fdb0291b3c6daf8d1ce122c3
Ric-Lavers <ric_lavers@outlook.com>
2020-05-05 21:24:00 +1000

    test

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
-require("dotenv").config()
-import "babel-polyfill"
-import express from "express"
-import bodyParser from "body-parser"
+require("dotenv").config();
+import "babel-polyfill";
+import express from "express";
+import bodyParser from "body-parser";
 
-import demoPaths from "./data/routes/demoPaths"
-import skibblPaths from "./data/routes/skribbl.routes"
+import demoPaths from "./data/routes/demoPaths";
+import skibblPaths from "./data/routes/skribbl.routes";
 
-const cors = require("cors")
-const app = express()
+const cors = require("cors");
+const app = express();
 
-app.use(cors('*'))
-app.use(bodyParser.json({
-  type: "*/*"
-}))
+app.use(cors("*"));
+app.use(
+  bodyParser.json({
+    type: "*/*",
+  })
+);
 app.use(
   bodyParser.urlencoded({
     extended: true,
-  }),
-)
+  })
+);
 
 app.get("/", (req, res) => {
-  return res.status(200).json("HOME")
-})
+  return res.status(200).json("HOME");
+});
 
-app.post("/demo", (req, res) => demoPaths.postDemo(req, res))
-app.use("/skibbl", skibblPaths)
+app.post("/demo", (req, res) => demoPaths.postDemo(req, res));
+// app.use("/skibbl", skibblPaths);
 
-app.listen(8080, () => console.log("Running server on port localhost:8080"))
+app.listen(8080, () => console.log("Running server on port localhost:8080"));
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
